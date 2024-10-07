9022f96557b6f8e665de7baf3de2937afce39e09
Nakib <nakkibcse@gmail.com>
2024-10-07 12:44:53 +0600

    Solved BreakNumber and HadyRidesTheTrain

---
+++ Break Number/Program.cs
@@ -0,0 +1,16 @@
+﻿long n = long.Parse(Console.ReadLine());
+long[] inputs = Array.ConvertAll(Console.ReadLine().Split(' '), long.Parse);
+
+for (int i = 0; i < n; i++)
+{
+    long x = inputs[i];
+    int cnt = 0;
+    while (x % 2 == 0)
+    {
+        x = x/ 2; 
+        cnt++;
+    }
+    inputs[i] = cnt;
+}
+Array.Sort(inputs);
+Console.WriteLine(inputs[n-1]);
---
+++ Hady Rides the Train/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    long x = long.Parse(Console.ReadLine());
+
+    long row = x / 4;
+    long col;
+
+    if (row % 2 == 0)
+    {
+        col = x % 4;
+    }
+    else
+    {
+        col = 3 - (x % 4);
+    }
+    Console.WriteLine($"{row} {col}");
